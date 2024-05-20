1aac9fadc824d75cc6a4f4122db97807f3970a5b
Wojciech Galecki <wojciech.galecki@wp.pl>
2024-05-20 14:32:47 +0200

    initial quarkus app setup

---
+++ src/main/java/wg/app/model/Film.java
@@ -0,0 +1,194 @@
+package wg.app.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "film", schema = "sakila")
+public class Film {
+
+    public Film() {}
+
+    public Film(Long filmId, String title, Long length) {
+        this.filmId = filmId;
+        this.title = title;
+        this.length = length;
+    }
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "film_id")
+    private Long filmId;
+    @Basic
+    @Column(name = "title")
+    private String title;
+    @Basic
+    @Column(name = "description")
+    private String description;
+    @Basic
+    @Column(name = "language_id")
+    private Long languageId;
+    @Basic
+    @Column(name = "original_language_id")
+    private Short originalLanguageId;
+    @Basic
+    @Column(name = "rental_duration")
+    private short rentalDuration;
+    @Basic
+    @Column(name = "rental_rate", columnDefinition = "decimal(4,2)")
+    private Float rentalRate;
+    @Basic
+    @Column(name = "length")
+    private Long length;
+    @Basic
+    @Column(name = "replacement_cost")
+    private BigDecimal replacementCost;
+    @Basic
+    @Column(name = "rating", columnDefinition = "enum('G', 'PG', 'PG-13', 'R', 'NC-17')")
+    private String rating;
+    @Basic
+    @Column(name = "special_features", columnDefinition = "set('Trailers', 'Commentaries', 'Deleted Scenes', 'Behind the Scenes')")
+    private String specialFeatures;
+    @Basic
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(
+        name = "film_actor",
+        joinColumns = { @JoinColumn(name = "film_id") },
+        inverseJoinColumns = { @JoinColumn(name = "actor_id") }
+    )
+    private List<Actor> actors = new ArrayList<>();
+
+    public Long getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(Long filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Long languageId) {
+        this.languageId = languageId;
+    }
+
+    public Short getOriginalLanguageId() {
+        return originalLanguageId;
+    }
+
+    public void setOriginalLanguageId(Short originalLanguageId) {
+        this.originalLanguageId = originalLanguageId;
+    }
+
+    public short getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(short rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public Float getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(Float rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public Long getLength() {
+        return length;
+    }
+
+    public void setLength(Long length) {
+        this.length = length;
+    }
+
+    public BigDecimal getReplacementCost() {
+        return replacementCost;
+    }
+
+    public void setReplacementCost(BigDecimal replacementCost) {
+        this.replacementCost = replacementCost;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getSpecialFeatures() {
+        return specialFeatures;
+    }
+
+    public void setSpecialFeatures(String specialFeatures) {
+        this.specialFeatures = specialFeatures;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return Objects.equals(filmId, film.filmId) && languageId == film.languageId && rentalDuration == film.rentalDuration && Objects.equals(title, film.title) && Objects.equals(description, film.description) && Objects.equals(originalLanguageId, film.originalLanguageId) && Objects.equals(rentalRate, film.rentalRate) && Objects.equals(length, film.length) && Objects.equals(replacementCost, film.replacementCost) && Objects.equals(rating, film.rating) && Objects.equals(specialFeatures, film.specialFeatures) && Objects.equals(lastUpdate, film.lastUpdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(filmId, title, description, languageId, originalLanguageId, rentalDuration, rentalRate, length, replacementCost, rating, specialFeatures, lastUpdate);
+    }
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+}
